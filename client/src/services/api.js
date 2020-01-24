b80ddece7e0d5152ae06a9ff0264d3d622cf460e
Cripto Girl <natalie.seltzer@gmail.com>
2020-01-24 18:07:32 +0000

    changing axios config to server url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -11,7 +11,8 @@ import storeAuth from '../store/store-auth'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`,
+    // baseURL: `http://localhost:8081/`,
+    baseURL: `http://167.172.59.209:8081/`,
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${storeAuth.state.authToken}`
